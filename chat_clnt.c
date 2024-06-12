2af499993775aee7bbf13e767d7380ac8a899da0
dongyun <ehddbs452100@naver.com>
2024-06-12 20:34:24 +0900

    chatting client

---
+++ chat_clnt.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
+
+#define BUF_SIZE 100
+#define NAME_SIZE 20
+
+void *send_msg(void *arg);
+void *recv_msg(void *arg);
+void error_handling(char *msg);
+void welcome_message();
+
+char name[NAME_SIZE] = "[DEFAULT]";
+char msg[BUF_SIZE];
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    struct sockaddr_in serv_addr;
+    pthread_t snd_thread, rcv_thread;
+    void *thread_return;
+
+    if (argc != 4) {
+        printf("Usage : %s <IP> <port> <name>\n", argv[0]);
+        exit(1);
+    }
+
+    sprintf(name, "%s", argv[3]);
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("connect() error");
+
+    write(sock, name, strlen(name));
+
+    pthread_create(&snd_thread, NULL, send_msg, (void *)&sock);
+    pthread_create(&rcv_thread, NULL, recv_msg, (void *)&sock);
+    pthread_join(snd_thread, &thread_return);
+    pthread_join(rcv_thread, &thread_return);
+    close(sock);
+    return 0;
+}
+
+void *send_msg(void *arg)
+{
+    int sock = *((int *)arg);
+    char name_msg[NAME_SIZE + BUF_SIZE];
+
+    welcome_message();
+
+    while (1) {
+        fgets(msg, BUF_SIZE, stdin);
+        if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n")) {
+            close(sock);
+            exit(0);
+        }
+
+        snprintf(name_msg, sizeof(name_msg), "%s %s", name, msg);
+        write(sock, name_msg, strlen(name_msg));
+    }
+    return NULL;
+}
+
+void *recv_msg(void *arg)
+{
+    int sock = *((int *)arg);
+    char name_msg[NAME_SIZE + BUF_SIZE];
+    int str_len;
+
+    while (1) {
+        memset(name_msg, 0, sizeof(name_msg)); 
+        str_len = read(sock, name_msg, sizeof(name_msg) - 1);
+        if (str_len == -1)
+            return (void *)-1;
+
+        name_msg[str_len] = 0;
+        fputs(name_msg, stdout);
+    }
+    return NULL;
+}
+
+void error_handling(char *msg)
+{
+    fputs(msg, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+void welcome_message()
+{
+    printf("=========Temporary Room=========\n");
+    printf("\n 기능 리스트 (문자열)\n\n\n");
+    printf("m : 방 만들기\n");
+    printf("e 방번호 : 해당 방번호 접속하기 \t ex) e 1\n");
+    printf("o : 방 나가기\n");
+    printf("q : 채팅웹 종료\n");
+    printf("@a 사용자명: 해당 사용자에게 귓속말하기 \t ex) @동윤 hello\n");
+    printf("\n================================\n\n\n");
+}
